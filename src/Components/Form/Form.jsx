7c718ae7b0569872cf55493ec4d820591cb9f9d7
khamrayev5636 <khamrayev5636@gmail.com>
2023-07-05 19:57:53 +0500

    Add css style

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -7,14 +7,14 @@ const Form = () => {
     <section className="hero py-5">
     <div className="container">
     <form className="form d-flex flex-column flex-md-row justify-content-md-between" method="GET">
-    <input className="search__input" type="search" name="countries_search" aria-label="Countries search"  placeholder="Search for a country…"/>
+    <input className="search__input mb-3 mb-md-0" type="search" name="countries_search" aria-label="Countries search"  placeholder="Search for a country…"/>
     <select className="form__select" aria-label="Default select example">
     <option selected hidden>Filter by Region</option>
-    <option value="1">Africa</option>
-    <option value="2">America</option>
-    <option value="3">Asia</option>
-    <option value="3">Europe</option>
-    <option value="3">Oceania</option>
+    <option value="africa">Africa</option>
+    <option value="america">America</option>
+    <option value="asia">Asia</option>
+    <option value="europe">Europe</option>
+    <option value="oceania">Oceania</option>
     </select>
     </form>
     </div>
